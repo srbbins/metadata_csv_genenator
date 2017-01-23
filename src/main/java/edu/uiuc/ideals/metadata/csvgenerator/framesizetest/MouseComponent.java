93279bbc69abace3ecb66b9ffd492602b06d2a98
Robbins <srobbins@wirelessprv-10-192-22-92.near.illinois.edu>
2017-01-23 15:57:15 -0600

    remove frame size test code

--- src/main/java/edu/uiuc/ideals/metadata/csvgenerator/framesizetest/MouseComponent.java
+++
@@ -1,7 +0,0 @@
-package edu.uiuc.ideals.metadata.csvgenerator.framesizetest;
-
-/**
- * Created by srobbins on 1/8/17.
- */
-public class MouseComponent {
-}
--- src/main/java/edu/uiuc/ideals/metadata/csvgenerator/framesizetest/NotHelloWorldComponent.java
+++
@@ -1,7 +0,0 @@
-package edu.uiuc.ideals.metadata.csvgenerator.framesizetest;
-
-/**
- * Created by srobbins on 1/7/17.
- */
-public class NotHelloWorldComponent {
-}
